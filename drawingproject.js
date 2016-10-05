5e2cea8ad87f05651fb2285c546fcb5f5ee747a0
Bryan Lopez <lopezz.bryann966@gmail.com>
2016-10-05 11:14:10 -0700

    in

--- drawingproject.js
+++ drawingproject.js
@@ -1,10 +1,16 @@
 var canvasDiv = document.getElementById("drawingArea");
 var canvas = canvasDiv.getContext('2d');
 
+canvas.beginPath();
+canvas.moveTo(75,50);
+canvas.lineTo(100,75);
+canvas.lineTo(100,25);
+canvas.fill();
 
-canvas.fillStyle = "black";
-canvas.fillRect(80,150,100,100);
-var c=document.getElementById("drawingArea");
-var ctx=c.getContext("2d");
-ctx.strokeStyle="#FF0000";
-ctx.strokeRect(20,20,150,100);
+canvas.beginPath();
+canvas.arc(100, 75, 50, 0, 2*Math.PI);
+canvas.stroke();
+
+
+canvas.fillstyle = "grey";
+canvas.fillRect(0,0,2000,2000);
