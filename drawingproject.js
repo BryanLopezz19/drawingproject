f3c7d8876aaa703df95ca62e578eb21b133b79e9
Bryan Lopez <lopezz.bryann966@gmail.com>
2016-10-06 11:26:27 -0700

    b

--- drawingproject.js
+++ drawingproject.js
@@ -1,5 +1,15 @@
 var canvasDiv = document.getElementById("drawingArea");
 var canvas = canvasDiv.getContext('2d');
 
-canvas.fillstyle = "grey";
+/*Background*/
+canvas.fillStyle = "grey";
 canvas.fillRect(0,0,2000,2000);
+
+canvas.fillStyle = "black";
+canvas.fillRect(700,300,20,20);
+canvas.fillRect(700,320,20,20);
+canvas.fillRect(700,280,20,20);
+canvas.fillRect(720,260,20,20);
+canvas.fillRect(740,260,20,20);
+canvas.fillRect(750,280,20,20);
+canvas.fillRect(770,280,20,20);
