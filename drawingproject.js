27b50d414466641653868d626ca3f96688bb553a
Bryan Lopez <lopezz.bryann966@gmail.com>
2016-10-11 10:58:54 -0700

    idk
    random stuff i guess

--- drawingproject.js
+++ drawingproject.js
@@ -2,7 +2,7 @@ var canvasDiv = document.getElementById("drawingArea");
 var canvas = canvasDiv.getContext('2d');
 
 /*Background*/
-canvas.fillStyle = "grey";
+canvas.fillStyle = "brown";
 canvas.fillRect(0,0,2000,2000);
 
 canvas.fillStyle = "black";
@@ -11,3 +11,14 @@ canvas.fillRect(720,320,20,20);
 canvas.fillRect(720,280,20,20);
 canvas.fillRect(740,300,20,20);
 canvas.fillRect(720,300,20,20);
+
+canvas.fillRect(500,300,20,20);
+canvas.fillRect(520,320,20,20);
+canvas.fillRect(520,280,20,20);
+canvas.fillRect(540,300,20,20);
+canvas.fillRect(520,300,20,20);
+
+canvas.beginPath();
+canvas.fillStyle = "white";
+canvas.arc(200,200,50,0,2*Math.PI,true);
+canvas.fill();  
